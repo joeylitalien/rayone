6cc5af0c214421e0341277906a29c85cc9891b77
joeylitalien <joey.litalien@gmail.com>
2016-09-13 02:34:43 -0400

    Reverted to initial vector implementation, doesn't depend on GLM

--- src/core/geometry.h
+++ src/core/geometry.h
@@ -3,21 +3,38 @@
 #define RT_CORE_GEOMETRY_H
 
 #include "raytracer.h"
-#include "../glm/glm.hpp"
-using glm::vec3;
 
-/**
- *   Ray class
- *   Origin o and Direction d: r(t) = o + td
- */
+// Vec class declaration
+// Vector u = (x, y, z)
+class Vec {
+public:
+  Vec() { x = y = z = 0.; }
+  Vec(const double &w) { x = y = z = w; }
+  Vec(const double &x_, const double &y_, const double &z_)
+    : x(x_), y(y_), z(z_) {}
+
+  // Operator overloading: Add, subtract and mulitply by scalar
+  Vec operator+(const Vec &v) const { return Vec(x + v.x, y + v.y, z + v.z); }
+  Vec operator-(const Vec &v) const { return Vec(x - v.x, y - v.y, z - v.z); }
+  Vec operator*(double s) const { return Vec(x * s, y * s, z * s); }
+
+  // Vector operations: dot, cross, point-wise multiplication and norm
+  double dot(const Vec &v) const { return x * v.x + y * v.y + z * v.z; }
+  Vec cross(const Vec &v) const {
+    return Vec(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+  }
+  Vec mult(const Vec &v) const { return Vec(x * v.x, y * v.y, z * v.z); }
+  Vec &norm() { return *this = *this * (1.0 / sqrt(x * x + y * y + z * z)); }
+
+  double x, y, z;
+};
+
+// Ray class declaration
+// Origin o and direction d: r(t) = o + td
 class Ray {
 public:
-  // Ray public methods
-  Ray(vec3 o_, vec3 d_) : o(o_), d(d_) {}
-  vec3 operator()(float t) const { return o + d * t; }
-
-  // Ray public data
-  vec3 o, d;
+  Ray(Vec o_, Vec d_) : o(o_), d(d_) {}
+  Vec o, d;
 };
 
 #endif   // RT_CORE_GEOMETRY_H
