93d6467fefd0a456ff60f9be4e616fb89f7d2521
joeylitalien <joey.litalien@gmail.com>
2016-09-15 18:28:04 -0400

    Added a function to save file, out of main()

--- src/core/pathtracer.h
+++ src/core/pathtracer.h
@@ -6,10 +6,11 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
 #include "geometry.h"
 #include "shape.h"
 #include "renderer.h"
-
+#include "montecarlo.h"
 
 // Global constants
 #define RR_DEPTH 5     // Number of bounce before Russian Roulette
@@ -20,6 +21,7 @@
 class Vec;
 class Ray;
 class Shape;
+struct Intersection;
 class Renderer;
 
 // Restrict x value to [0,1] interval
@@ -39,7 +41,7 @@ inline double RandomGen(unsigned short *seed) {
 }
 
 // Antialiasing tent filter (approximate sinc filter)
-inline void TentFilter(double &dx, double &dy, unsigned short int *Xi) {
+inline void TentFilter(double &dx, double &dy, unsigned short *Xi) {
   double a = 2 * RandomGen(Xi); dx = a < 1 ? sqrt(a) - 1 : 1 - sqrt(2 - a);
   double b = 2 * RandomGen(Xi); dy = b < 1 ? sqrt(b) - 1 : 1 - sqrt(2 - b);
 }
@@ -48,6 +50,19 @@ inline void TentFilter(double &dx, double &dy, unsigned short int *Xi) {
 inline void BuildOrthonormalFrame(Vec &u, Vec &v, Vec &w) {
   u = (fabs(w.x) > .1 ? Vec(0,1,0) : Vec(1,0,0)).Cross(w).Norm();
   v = w.Cross(u);
+}
+
+// Write image as PPM Portable Bitmap Format
+// See http://netpbm.sourceforge.net/doc/ppm.html for more info
+inline void SaveImage(char const *fileName, Vec *pixCol, int w, int h) {
+  FILE *f = fopen(fileName, "w");
+  fprintf(f, "P3\n%d %d\n%d\n", w, h, 255);
+  for (int p = 0; p < w * h; p++) {
+    fprintf(f, "%d %d %d ", ToDisplayValue(pixCol[p].x),
+                            ToDisplayValue(pixCol[p].y),
+                            ToDisplayValue(pixCol[p].z));
+  }
+  fclose(f);
 }
 
 #endif   // PT_CORE_PATHTRACER_H
